7dae158cbdf244064db0ba17c6f556eb54d2d844
Will Silveira <willianbs@gmail.com>
2025-04-23 11:49:44 -0300

    release config updates

--- release.config.js
+++ release.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  branches: ['master'],
+  branches: ['master', 'main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/git',
     '@semantic-release/changelog',
     [
       '@semantic-release/npm',
@@ -14,10 +13,18 @@ module.exports = {
     [
       '@semantic-release/github',
       {
-        assets: 'release/*.tgz'
+        assets: 'release/*.tgz',
+        addReleases: 'bottom'
       }
     ],
-    '@semantic-release/git'
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
   ],
   preset: 'angular'
 };
